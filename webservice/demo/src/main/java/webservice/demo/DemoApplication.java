f59e25485f7612ad2c488b10d6045d9a3b0ca41b
Skoro-Dani <Skorodaniel@gmail.com>
2022-04-08 13:56:49 +0200

    -

--- webservice/demo/src/main/java/webservice/demo/DemoApplication.java
+++ webservice/demo/src/main/java/webservice/demo/DemoApplication.java
@@ -1,16 +1,57 @@
 package webservice.demo;
 
+import java.beans.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class DemoApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		DBconnection.createNewDBconnection();
+
+		Statement stmt;
+		ResultSet results;
+
+		String sql_select = "Select * From utente";
+
+		try (Connection conn = DBconnection.createNewDBconnection()) {
+
+			stmt = (java.beans.Statement) conn.createStatement();
+			results = ((java.sql.Statement) stmt).executeQuery(sql_select);
+
+			List<utente> studentsList = new ArrayList<utente>();
+
+			while (results.next()) {
+
+				utente stdObject = new utente();
+
+				stdObject.setID(results.getString("ID"));
+				stdObject.setpassword(results.getString("username"));
+				stdObject.setusername(results.getString("password"));
+
+				studentsList.add(stdObject);
+			}
+
+			ObjectMapper mapper = new ObjectMapper();
+			String JSONOutput = mapper.writeValueAsString(studentsList);
+			System.out.println(JSONOutput);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 	}
 
-	
 }
---
+++ webservice/demo/src/main/java/webservice/demo/utente.java
@@ -0,0 +1,21 @@
+package webservice.demo;
+
+public class utente {
+    String ID,username,password;
+    public utente(){
+
+    }
+    public void setID(String id)
+    {
+        ID=id;
+    }
+    public void setusername(String username)
+    {
+        this.username=username;
+    }
+    public void setpassword(String password)
+    {
+        this.password=password;
+    }
+    
+}
